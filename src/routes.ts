8a9b1f150c054107edb227fda051fdec60d4094f
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-30 20:23:08 +0430

    Import dashboard page

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import React from 'react'
 import { RouteComponentProps, StaticContext } from 'react-router'
 const Home = React.lazy(() => import('./Views/Home/Home'))
+const Dashboard = React.lazy(() => import('./Views/Dashboard/Dashboard'))
 type TRoutes =
     {
         path: string,
@@ -11,5 +12,10 @@ const Routes: TRoutes = [{
     path: '/',
     exact: true,
     page: Home
+},
+{
+    path: '/dashboard',
+    exact: true,
+    page: Dashboard
 }]
 export default Routes;
